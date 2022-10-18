5abbdc9d42d04d0a329c2ca6901242f9e10138a4
wanjiku <annritawanjiku77@gmail.com>
2022-10-18 14:18:19 +0300

    complete

---
+++ app.rb
@@ -0,0 +1,23 @@
+
+
+puts "Hello World!"
+
+print "Pass this test, please."
+
+p [1,2,3]
+
+
+# # ruby equivalent of console.log
+# puts "hello world!"
+# puts "hello world!"
+# puts "hello world!"
+# puts "hello world!"
+# # ruby output methods
+# print "hello world"
+# print "hello world"
+# print "hello world"
+# # ruby output for things that are not strings
+# p [1,2,3]
+# puts [1,2,3].inspect
+# pp [{id:1, hello:"world"},{id:2, hello:"ruby"},{id:3, hello:"moon"},{id:4,hello:"learner"}]
+
